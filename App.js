2b232764745393bdaa42c1a64a5874dc1fe13872
hokosary <hokosary3472@gmail.com>
2026-05-29 22:37:41 +0300

    add main app file

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import AddHabitScreen from './screens/AddHabitScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ title: 'Список привычек' }} 
+        />
+        <Stack.Screen 
+          name="AddHabit" 
+          component={AddHabitScreen} 
+          options={{ title: 'Добавление привычки' }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
